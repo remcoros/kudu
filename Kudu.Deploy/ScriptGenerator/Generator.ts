fdedc88b201c09e2b5f01cb8ef40978522491500
Amit Apple <amitap@microsoft.com>
2012-10-30 18:51:37 -0700

    Added website custom script generation.

--- Kudu.Deploy/ScriptGenerator/Generator.ts
+++ Kudu.Deploy/ScriptGenerator/Generator.ts
@@ -7,6 +7,9 @@ function generateDeploymentScript(projectType: string, projectPath: string, solu
     if (projectType == "WAP") {
         generateWapDeploymentScript(projectPath, solutionPath);
     }
+    else if (projectType == "WEBSITE") {
+        generateWebSiteDeploymentScript(solutionPath);
+    }
     else {
         throw new Error("Invalid project type received: " + projectType);
     }
@@ -14,25 +17,37 @@ function generateDeploymentScript(projectType: string, projectPath: string, solu
 
 function generateWapDeploymentScript(projectPath: string, solutionPath: string) {
     Ensure.argNotNull(projectPath, "projectPath");
-    Ensure.argNotNull(solutionPath, "solutionPath");
+
+    var msbuildArguments = "\"" + projectPath + "\" /nologo /verbosity:m /t:pipelinePreDeployCopyAllFilesToOneFolder /p:_PackageTempDir=\"%TEMPORARY_PATH%\";AutoParameterizationWebConfigConnectionStrings=false;Configuration=Release /p:SolutionDir=\"%SOLUTION_PATH%\"";
+    if (solutionPath != null) {
+        msbuildArguments += " /p:SolutionDir=\"" + solutionPath + "\"";
+    }
+
+    generateDotNetDeploymentScript("deploy.wap.template", msbuildArguments);
+}
+
+function generateWebSiteDeploymentScript(solutionPath: string) {
+    if (solutionPath == null) {
+        throw new Error("The solution file path is required for .NET web site deployment script");
+    }
+
+    var msbuildArguments = "\"%SOLUTION_PATH%\" /verbosity:m /nologo";
+    generateDotNetDeploymentScript("deploy.website.template", msbuildArguments);
+}
+
+function generateDotNetDeploymentScript(templatePath: string, msbuildArguments: string) {
+    Ensure.argNotNull(templatePath, "templatePath");
 
     var winDir = process.env["WINDIR"];
     var msbuildPath = winDir + "\Microsoft.NET\Framework\v4.0.30319";
-    if (!fs.existsSync(msbuildPath)) {
+    /*if (!fs.existsSync(msbuildPath)) {
         msbuildPath = winDir + "\Microsoft.NET\Framework64\v4.0.30319";
-    }
+    }*/
 
-    var templateContent = fs.readFileSync("deploy.wap.template", "utf8");
-    var msbuildArguments = "/p:SolutionDir=\"%SOLUTION_PATH%\""
-
-    if (solutionPath != null) {
-        msbuildArguments += " /p:SolutionDir=\"%SOLUTION_PATH%\"";
-    }
+    var templateContent = fs.readFileSync(templatePath, "utf8");
 
     templateContent =
-        templateContent.replace("{ProjectPath}", projectPath)
-                       .replace("{SolutionPath}", solutionPath)
-                       .replace("{MSBuildArguments}", msbuildArguments)
+        templateContent.replace("{MSBuildArguments}", msbuildArguments)
                        .replace("{MSBuildPath}", msbuildPath);
 
     fs.writeFileSync("deploy.cmd", templateContent);
